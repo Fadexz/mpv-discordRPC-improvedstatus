b25480f233ad423004d3a053dea0bd839153ecb7
Fadexz <37004055+Fadexz@users.noreply.github.com>
2023-03-20 15:12:41 +1000

    Update status-line.lua
    Switches to elapsed time instead of time left for the time counter status and some other small modifications to the status

--- status-line.lua
+++ status-line.lua
@@ -12,18 +12,51 @@ function update_status_line()
     -- Reset the status line
     newStatus = ""
 
---[[
+    --[[
     if mp.get_property_bool("pause") then
         --atsl("") -- (Paused)
---]]
+    --]]
 
-    print("Hello World!")
+    --print("Hello World!")
 
     if mp.get_property_bool("paused-for-cache") then
         atsl("(Buffering) ")
     end
 
---[[
+    --[[
+    r = mp.get_property_number("cache", 0)
+    if r > 0 then
+        atsl(string.format(" Cache: %d%% ", r))
+    end
+    --]]
+
+    local dur_time = mp.get_property_osd("duration")
+	local ecl_time = mp.get_property_osd("time-pos")
+    local dur_hour, dur_minute, dur_second = dur_time:match("(%d+):(%d+):(%d+)")
+    local ecl_hour, ecl_minute, ecl_second = ecl_time:match("(%d+):(%d+):(%d+)")
+    if mp.get_property_number("time-pos") == nil or mp.get_property_number("time-pos") < 0 then
+        ecl_hour, ecl_minute, ecl_second = "00", "00", "00"
+    end
+    if dur_hour == "00" then
+        if tonumber(dur_minute) <= 9 then
+            dur_time = dur_minute:sub(2) .. ":" .. dur_second
+    		ecl_time = ecl_minute:sub(2) .. ":" .. ecl_second
+        else
+            dur_time = dur_minute .. ":" .. dur_second
+    		ecl_time = ecl_minute .. ":" .. ecl_second
+        end
+    elseif dur_hour ~= nil and tonumber(dur_hour) <= 9 then
+        dur_time = dur_hour:sub(2) .. ":" .. dur_minute .. ":" .. dur_second
+    	ecl_time = ecl_hour:sub(2) .. ":" .. ecl_minute .. ":" .. ecl_second
+    end
+    atsl(--[[ecl_time .. "/" ..--]] dur_time .. " (" .. mp.get_property_osd("percent-pos", -1) .. "%)")
+
+    local r = mp.get_property_number("speed", -1)
+    if r ~= 1 then
+        atsl(" (x" .. r .. ")")
+    end
+
+    --[[
     if mp.get_property("aid") ~= "no" then
         atsl("A")
     end
@@ -32,109 +65,8 @@ function update_status_line()
     end
 
     atsl(": ")
---]]
-	
-	
-    local duration = mp.get_property_number("duration")
-    local dur_hours = math.floor(duration / 3600)
-    local dur_minutes = math.floor(duration / 60)
-    local dur_seconds = math.floor(duration % 60)
-	if dur_hours >= 1 then
-        if dur_hours < 10 then
-            dur_hours = "0" .. tostring(dur_hours) .. ":"
-        else dur_hours = tostring(dur_hours) .. ":"
-		end
-	elseif dur_hours <= 0 then
-	    dur_hours = ""
-    end
-	if dur_minutes >= 1 then
-        if dur_minutes < 10 and dur_hours ~= "" then
-            dur_minutes = "0" .. tostring(dur_minutes) .. ":"
-        else dur_minutes = tostring(dur_minutes) .. ":"
-		end
-	elseif dur_minutes <= 0 then
-	    dur_minutes = ""
-	end
-	if dur_seconds < 0 then
-        dur_seconds = "00"
-	else dur_seconds = tostring(dur_seconds)
-	end
-    local dur_time = dur_hours .. dur_minutes .. string.format("%02d", dur_seconds)
+    --]]
 
-	local eclapsed = mp.get_property_number("time-pos")
-    local ecl_hours = math.floor(eclapsed / 3600)
-    local ecl_minutes = math.floor(eclapsed / 60)
-    local ecl_seconds = math.floor(eclapsed % 60)
-	if ecl_hours >= 1 then
-        if ecl_hours < 10 then
-            ecl_hours = "0" .. tostring(ecl_hours) .. ":"
-        else ecl_hours = tostring(ecl_hours) .. ":"
-		end
-	elseif ecl_hours <= 0 then
-	    ecl_hours = ""
-    end
-	if ecl_minutes >= 1 then
-        if ecl_minutes < 10 and dur_hours ~= ""  then
-            ecl_minutes = "0" .. tostring(ecl_minutes) .. ":"
-        else ecl_minutes = tostring(ecl_minutes) .. ":"
-		end
-	elseif dur_minutes ~= "" then
-	    ecl_minutes = tostring(ecl_minutes) .. ":"
-	elseif ecl_minutes <= 0 then
-	    ecl_minutes = ""
-	end
-	if ecl_seconds < 0 then
-        ecl_seconds = "00"
-	else ecl_seconds = tostring(ecl_seconds)
-	end
-
-    -- Note: Hour long timecode breaks currently and I think sometimes on shorter content even for some reason (too lazy to test and fix atm)
-
---[[
-	if dur_hours.len() == 3 and ecl_hours.len() ~= 3 then
-	    ecl_hours = "0" .. tostring(ecl_hours)
-	elseif dur_hours.len() == 2 and ecl_hours.len() ~= 2 then
-	    ecl_hours = "0:"
-	end
-	if dur_minutes.len() == 3 and ecl_minutes.len() ~= 3 then
-	    ecl_minutes = "0" .. tostring(ecl_minutes)
-	elseif dur_minutes.len() == 2 and ecl_minutes.len() ~= 2 then
-	    ecl_minutes = "0:"
-	end
-	if dur_seconds.len() == 3 and ecl_seconds.len() ~= 3 then
-	    ecl_seconds = "0" .. tostring(ecl_seconds)
-	elseif dur_seconds.len() == 2 and ecl_seconds.len() ~= 2 then
-	    ecl_seconds = "0:"
-	end
---]]
-
-    local ecl_time = ecl_hours .. ecl_minutes .. string.format("%02d", ecl_seconds)
-
-
-    atsl(ecl_time .. "/" .. dur_time .. " (" .. mp.get_property_osd("percent-pos", -1) .. "%)")
-
-    local r = mp.get_property_number("speed", -1)
-    if r ~= 1 then
-        atsl(" (x" .. r .. ") ")
-    end
-
---[[
-    r = mp.get_property_number("avsync", nil)
-    if r ~= nil then
-        atsl(string.format(" A-V: %f", r))
-    end
-
-    r = mp.get_property("total-avsync-change", 0)
-    if math.abs(r) > 0.05 then
-        atsl(string.format(" ct:%7.3f", r))
-    end
-	
-    r = mp.get_property_number("decoder-drop-frame-count", -1)
-    if r > 0 then
-        atsl(" Late: " .. r)
-    end
---]]
-	
     if mp.get_property_osd("video-format") ~= "mjpeg" and mp.get_property_osd("video-format") ~= "png" and mp.get_property("width") ~= nil and mp.get_property("height") ~= nil then
         if mp.get_property_number("width") == 1920 and mp.get_property_number("height") <= 1080 then
             r = "1080p"
@@ -148,6 +80,12 @@ function update_status_line()
             r = "5K"
         elseif mp.get_property_number("width") == 7680 and mp.get_property_number("height") <= 4320 then
             r = "8K"
+		--[[
+		elseif mp.get_property_number("width") == 11520 and mp.get_property_number("height") <= 6480 then
+            r = "12K"
+		elseif mp.get_property_number("width") == 15360 and mp.get_property_number("height") <= 8640 then
+            r = "16K"
+		--]]
         else r = mp.get_property_osd("height") .. "p"
         end
         if r ~= nil and r ~= "" then
@@ -155,15 +93,24 @@ function update_status_line()
         end
     end
 	
-    r = mp.get_property_osd("estimated-vf-fps")
-    if r ~= nil and r ~= "" and mp.get_property_osd("video-format") ~= "mjpeg" and mp.get_property_osd("video-format") ~= "png" then
-        atsl(" " .. r * 1 .. "fps")
+    r = mp.get_property_number("container-fps")
+    if r ~= nil and mp.get_property_osd("video-format") ~= "mjpeg" and mp.get_property_osd("video-format") ~= "png" then
+        atsl(" " .. round(r) .. "fps")
+		if mp.get_property_number("estimated-vf-fps") ~= nil and mp.get_property_number("estimated-vf-fps") >= r * 2 - 1 then
+		    atsl(" (" .. round(mp.get_property_number("estimated-vf-fps")) .. "fps)")
+		end
     end
 	
     r = mp.get_property_osd("video-format")
     if r ~= nil and r ~= "" and mp.get_property_osd("video-format") ~= "mjpeg" and mp.get_property_osd("video-format") ~= "png" then
 	    if r == "prores" then
 		    r = "ProRes"
+		elseif r == "dnxhd" then
+		    r = "DNxHD"
+		elseif r == "dnxhr" then
+		    r = "DNxHR"
+		elseif r == "cfhd" then
+		    r = "CineForm"
 		else r = r:upper()
 		end
         atsl(" " .. r)
@@ -181,7 +128,19 @@ function update_status_line()
 	
     r = mp.get_property_osd("audio-codec-name")
     if r ~= nil and r ~= "" then
-        atsl(" " .. r:upper())
+	    if r == "opus" then
+		    r = "Opus"
+	    elseif r == "vorbis" then
+		    r = "Vorbis"
+		elseif string.match(r, "^pcm_") then
+		    r = "PCM"
+		--[[
+		elseif r == "truehd" then
+		    r = "TrueHD"
+		--]]
+		else r = r:upper()
+		end
+        atsl(" " .. r)
     end
 	
     r = mp.get_property_osd("packet-audio-bitrate")
@@ -190,24 +149,18 @@ function update_status_line()
     end
 
     r = mp.get_property_number("file-size")
-    if r ~= nil and r ~= "" then
-	    if r < 1000000000 then
-		    if r < 1000000 then
-			    atsl(" " .. tonumber(tostring(string.format("%.1f", r/1048000)))*1 .. " MiB")
-			else atsl(" " .. tonumber(tostring(math.floor((r/1048000) + 0.5)))*1 .. " MiB")
-			end
-		elseif r < 10000000000 then
-		    atsl(" " .. tonumber(tostring(string.format("%.1f", r/1072000000)))*1 .. " GiB")
-		else atsl(" " .. tonumber(tostring(math.floor((r/1072000000) + 0.5)))*1 .. " GiB")
-		end
+    if r then
+        if r < 1048576000 then
+            if r < 976562 then
+                atsl(string.format(" %.1f MiB", r / 1048576))
+            else atsl(string.format(" %d MiB", math.floor(r / 1048576 + 0.5)))
+            end
+        elseif r < 10737418240 then
+            atsl(string.format(" %.1f GiB", r / 1073741824))
+        else atsl(string.format(" %d GiB", math.floor(r / 1073741824 + 0.5)))
+        end
     end
 
---[[
-    r = mp.get_property_number("cache", 0)
-    if r > 0 then
-        atsl(string.format(" Cache: %d%% ", r))
-    end
---]]
 
     -- Set the new status line
     mp.set_property("options/term-status-msg", newStatus)
@@ -221,7 +174,15 @@ function on_pause_change(name, value)
     else
         timer:stop()
     end
-    mp.add_timeout(0.1, update_status_line)
+    mp.add_timeout(1, update_status_line)
 end
 mp.observe_property("pause", "bool", on_pause_change)
 mp.register_event("seek", update_status_line)
+
+function round(num)
+    local rounded = string.format("%.3f", num)
+    if rounded:sub(-3) == "000" then
+        return string.format("%.0f", num)
+    else return rounded
+    end
+end
