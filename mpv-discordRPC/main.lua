457f4f25f7d7433da8dabcf742be83d66aa35b3b
Fadexz <37004055+Fadexz@users.noreply.github.com>
2023-03-20 15:24:37 +1000

    Update main.lua
    Adds the newer Discord GameSDK support
    Support image urls for cover art
    Added Genre and Date (year) support to title metadata
    Fixed status not showing for a playlist
    Playlist status will only show if there is more than one item
    Loop status will only show when enabled
    Fixed error when used with SVP so now the status will show with it
    Fixed error causing the status not to work with YouTube links
    Added 5 second delay until it will start to allow for time to disable the script (takes an extra 2~3 seconds to display on Discord)

--- mpv-discordRPC/main.lua
+++ mpv-discordRPC/main.lua
@@ -9,8 +9,9 @@ local msg = require 'mp.msg'
 
 -- set [options]
 local o = {
-	rpc_wrapper = "lua-discordRPC",
+	rpc_wrapper = "lua-discordGameSDK",
 	-- Available option, to set `rpc_wrapper`:
+	-- * lua-discordGameSDK
 	-- * lua-discordRPC
 	-- * python-pypresence
 	periodic_timer = 15,
@@ -39,18 +40,8 @@ local o = {
 }
 options.read_options(o)
 
--- set `script_info`
-local script_info = {
-	name = mp.get_script_name(),
---[[
-	description = "Discord Rich Presence integration for mpv Media Player",
-	upstream = "https://github.com/cniw/mpv-discordRPC",
-	version = "1.4.1",
---]]
-}
-
 -- set `mpv_version`
-local mpv_version = mp.get_property("mpv-version"):sub(5)
+local mpv_version = mp.get_property("mpv-version"):gsub("-.*", "")
 
 -- set `startTime`
 local startTime = os.time(os.date("*t"))
@@ -61,14 +52,22 @@ local function main()
 	local metadataTitle = mp.get_property_native("metadata/by-key/Title")
 	local metadataArtist = mp.get_property_native("metadata/by-key/Artist")
 	local metadataAlbum = mp.get_property_native("metadata/by-key/Album")
+	local metadataGenre = mp.get_property_native("metadata/by-key/Genre")
+	local metadataDate = mp.get_property_native("metadata/by-key/Date")
 	if metadataTitle ~= nil then
 		details = metadataTitle
 	end
 	if metadataArtist ~= nil then
-		details = ("%s\nby %s"):format(details, metadataArtist)
+		details = ("%s by %s"):format(details, metadataArtist)
 	end
 	if metadataAlbum ~= nil then
-		details = ("%s\non %s"):format(details, metadataAlbum)
+		details = ("%s on %s"):format(details, metadataAlbum)
+	end
+	if metadataGenre ~= nil then
+		details = ("%s [%s]"):format(details, metadataGenre)
+	end
+	if metadataDate ~= nil then
+		details = ("%s (%s)"):format(details, string.match(metadataDate, "(%d%d%d%d)"))
 	end
 	if details == nil then
 		details = "No file"
@@ -100,12 +99,12 @@ local function main()
 	if not idle then
 		-- set `playlist_info`
 		local playlist = ""
-		if o.playlist_info == "yes" then
+		if o.playlist_info == "yes" and mp.get_property_number("playlist-count") > 1 then
 			playlist = (" - Playlist: [%s/%s]"):format(mp.get_property("playlist-pos-1"), mp.get_property("playlist-count"))
 		end
 		-- set `loop_info`
 		local loop = ""
-		if o.loop_info == "yes" then
+		if o.loop_info == "yes" and mp.get_property_bool("loop-file") ~= false then
 			local loopFile = mp.get_property_bool("loop-file") == false and "" or "file"
 			local loopPlaylist = mp.get_property_bool("loop-playlist") == false and "" or "playlist"
 			if loopFile ~= "" then
@@ -144,8 +143,11 @@ local function main()
 				local lower_title = title[j] ~= nil and title[j]:lower() or ""
 				local lower_details = details ~= nil and details:lower() or ""
 				if lower_details:find(lower_title, 1, true) ~= nil then
-					local number = catalogs[i].number
-					largeImageKey = ("coverart_%s"):format(number):gsub("[ /~]", "_"):lower()
+					local cover_id_or_url = catalogs[i].cover_id_or_url
+					if cover_id_or_url.match(cover_id_or_url, "^https?://[%w-_%.%?%.:%/%+=&]+$") then
+						largeImageKey = cover_id_or_url
+					else largeImageKey = ("cover_%s"):format(cover_id_or_url):gsub("[^%w%- ]", "_"):lower()
+					end
 					largeImageText = title[j]
 				end
 			end
@@ -159,8 +161,11 @@ local function main()
 						local lower_artist = artist[k] ~= nil and artist[k]:lower() or ""
 						local lower_metadataArtist = metadataArtist ~= nil and metadataArtist:lower() or ""
 						if lower_artist == lower_metadataArtist then
-							local number = catalogs[i].number
-							largeImageKey = ("coverart_%s"):format(number):gsub("[ /~]", "_"):lower()
+							local cover_id_or_url = catalogs[i].cover_id_or_url
+							if cover_id_or_url.match(cover_id_or_url, "^https?://[%w-_%.%?%.:%/%+=&]+$") then
+								largeImageKey = cover_id_or_url
+							else largeImageKey = ("cover_%s"):format(cover_id_or_url):gsub("[^%w%- ]", "_"):lower()
+							end
 							largeImageText = album[j]
 						end
 					end
@@ -169,11 +174,11 @@ local function main()
 		end
 	end
 	-- streaming mode
-	local url = mp.get_property("path")
-	local stream = mp.get_property("stream-path")
+	local url = tostring(mp.get_property("path")):sub(1,127)
+	local stream = tostring(mp.get_property("stream-path")):sub(1,127)
 	if url ~= nil then
 		-- checking protocol: http, https
-		if string.match(url, "^https?://.*") ~= nil and o.hide_url == "no"  then
+		if string.match(url, "^https?://.*") ~= nil and o.hide_url == "no" then
 			largeImageKey = "mpv_stream"
 			largeImageText = url
 		elseif o.hide_url == "yes" then
@@ -190,17 +195,16 @@ local function main()
 		elseif string.match(url, "soundcloud.com/.+/.*$") ~= nil then
 			largeImageKey = "soundcloud"	-- alternative "soundcloud_big"
 			largeImageText = "SoundCloud"
-		elseif string.match(url, "listen.moe/.*stream$") ~= nil or string.match(url, "listen.moe/.*opus$") ~= nil or string.match(url, "listen.moe/.*fallback$") ~= nil or string.match(url, "listen.moe/.*m3u$") ~= nil then
-			largeImageKey = "listen_moe"	-- alternative "listen_moe_big"
-			largeImageText = string.match(url, "kpop") ~= nil and "LISTEN.moe - KPOP" or "LISTEN.moe - JPOP"
 		end
 	end
+
 	-- set `presence`
 	local presence = {
 		state = state,
 		details = details,
-		-- startTimestamp = math.floor(startTime),
-		endTimestamp = math.floor(timeUp),
+		--startTimestamp = math.floor(startTime),
+		startTimestamp = math.floor(timeNow - os.time(os.date("*t", mp.get_property("time-pos")))),
+		--endTimestamp = math.floor(timeUp),
 		largeImageKey = largeImageKey,
 		largeImageText = largeImageText,
 		smallImageKey = smallImageKey,
@@ -208,25 +212,35 @@ local function main()
 	}
 	if url ~= nil and stream == nil then
 		presence.state = "(Loading)"
-		presence.startTimestamp = math.floor(startTime)
+		presence.startTimestamp = math.floor(math.floor(timeNow - os.time(os.date("*t", mp.get_property("time-pos")))))
 		presence.endTimestamp = nil
 	end
 	if idle then
 		presence = {
 			state = presence.state,
 			details = presence.details,
-			startTimestamp = math.floor(startTime),
-			-- endTimestamp = presence.endTimestamp,
+			startTimestamp = math.floor(math.floor(timeNow - os.time(os.date("*t", mp.get_property("time-pos"))))),
+			--endTimestamp = presence.endTimestamp,
 			largeImageKey = presence.largeImageKey,
 			largeImageText = presence.largeImageText,
 			smallImageKey = presence.smallImageKey,
 			smallImageText = presence.smallImageText
 		}
 	end
+	
+	local appId = "1070587101088845875" --448016723057049601
 	-- run Rich Presence
-	if tostring(o.rpc_wrapper) == "lua-discordRPC" then
-		-- run Rich Presence with lua-discordRPC
-		local appId = "448016723057049601"
+	if tostring(o.rpc_wrapper) == "lua-discordGameSDK" then
+	    local appId = 1070587101088845875LL --448016723057049601
+		discord_instance = gameSDK.initialize(app_id)
+		if o.active == "yes" then
+			presence.details = presence.details:len() > 127 and presence.details:sub(1, 127) or presence.details
+			discord_instance = gameSDK.updatePresence(discord_instance, presence)
+		else
+			discord_instance = gameSDK.clearPresence(discord_instance)
+		end
+	elseif tostring(o.rpc_wrapper) == "lua-discordRPC" then
+	    local appId = "1070587101088845875" --448016723057049601
 		local RPC = require(o.rpc_wrapper)
 		RPC.initialize(appId, true)
 		if o.active == "yes" then
@@ -244,7 +258,6 @@ local function main()
 		if lib == "dll" then
 			pythonPath = pythonPath:gsub("/","\\\\")
 		end
-		-- run Rich Presence with pypresence
 		local todo = idle and "idle" or "not-idle"
 		local command = ('python "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s"'):format(pythonPath, todo, presence.state, presence.details, math.floor(startTime), math.floor(timeUp), presence.largeImageKey, presence.largeImageText, presence.smallImageKey, presence.smallImageText, o.periodic_timer)
 		mp.register_event('shutdown', function()
@@ -258,13 +271,6 @@ local function main()
 		end
 	end
 end
-
--- print script info
---[[
-msg.info(string.format(script_info.description))
-msg.info(string.format("Upstream: %s", script_info.upstream))
---]]
-msg.info(string.format("Version: %s", script_info.version))
 
 -- print option values
 msg.verbose(string.format("rpc_wrapper    : %s", o.rpc_wrapper))
@@ -280,10 +286,13 @@ msg.verbose(string.format("key_toggle     : %s", o.key_toggle))
 mp.add_key_binding(o.key_toggle, "active-toggle", function()
 		o.active = o.active == "yes" and "no" or "yes"
 		local status = o.active == "yes" and "active" or "inactive"
-		mp.osd_message(("[%s] Status: %s"):format(script_info.name, status))
+		mp.osd_message(("[%s] Status: %s"):format(mp.get_script_name(), status))
 		msg.info(string.format("Status: %s", status))
 	end,
 	{repeatable=false})
 
 -- run `main` function
-mp.add_periodic_timer(o.periodic_timer, main)
+mp.add_timeout(5, function()
+   main()
+   mp.add_periodic_timer(o.periodic_timer, main)
+end)
